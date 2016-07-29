63d210ea180711250b52a66a23c234ec5e374c51
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-07-28 18:07:32 -0700

    add controllers in app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+animateApp.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'page-home.html',
+    controller: 'mainController'
+  })
+
+  .when('/about', {
+    templateUrl: 'page-about.html',
+    controller: 'aboutController'
+  })
+
+  .when('/contact', {
+    templateUrl: 'page-contact.html',
+    controller: 'contactController'
+  });
+
+});
+
+//controllers
+
+animateApp.controller('mainController', function($scope){
+  $scope.pageClass = 'page-home';
+});
+
+animateApp.controller('aboutController', function($scope){
+  $scope.pageClass = 'page-about';
+});
+
+animateApp.controller('contactController', function($scope){
+  $scope.pageClass = 'page-contact';
+});
